58908b3a109f2464c0e9fafe91b8f79d87c81c40
Stefano Fiumara <stefano.fiumara@slalom.com>
2019-06-16 14:31:06 -0500

    Initialize DOTween in Input Handler

--- src/Assets/Scripts/HarryPotter/DebugTesting/InputHandler.cs
+++ src/Assets/Scripts/HarryPotter/DebugTesting/InputHandler.cs
@@ -1,4 +1,5 @@
-﻿using HarryPotter.Enums;
+﻿using DG.Tweening;
+using HarryPotter.Enums;
 using HarryPotter.Game.Cards;
 using HarryPotter.Game.Player;
 using UnityEngine;
@@ -6,13 +7,16 @@ using UnityEngine;
 namespace HarryPotter.DebugTesting
 {
     [RequireComponent(typeof(PlayerView))]
-    public class DebugInputManager : MonoBehaviour
+    public class InputHandler : MonoBehaviour
     {
         private PlayerView _player;
         private Camera _camera;
 
+        // TODO: State machine for Input Mode ?? (Selecting targets, playing from hand, etc.)
+
         private void Awake()
         {
+            DOTween.Init().SetCapacity(500, 500);
             _player = GetComponent<PlayerView>();
             _camera = Camera.main;
         }
@@ -22,14 +26,14 @@ namespace HarryPotter.DebugTesting
             if (Input.GetMouseButtonDown(0))
             {
                 var ray = _camera.ScreenPointToRay(Input.mousePosition);
-
+                
                 if (Physics.Raycast(ray, out var hitInfo))
                 {
                     var card = hitInfo.transform.GetComponent<CardView>();
-
+                    
                     if (card != null)
                     {
-                        _player.MoveToZone(card, Zone.Hand);
+                        _player.MoveToZone(card, Zone.Hand); // TEMP
                     }
                 }
             }
