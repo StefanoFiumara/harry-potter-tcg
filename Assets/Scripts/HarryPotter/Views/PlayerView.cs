1f00d7703e614a3dc45abbb20999265ee5ece59a
Stefano Fiumara <stefano.fiumara@slalom.com>
2020-02-09 23:37:31 -0600

    Notes

--- Assets/Scripts/HarryPotter/Views/PlayerView.cs
+++ Assets/Scripts/HarryPotter/Views/PlayerView.cs
@@ -6,5 +6,7 @@ namespace HarryPotter.Views
     public class PlayerView : MonoBehaviour
     {
         public Player Player;
+        
+        //TODO: Grab Zones from child components and use this view to handle animations between zones for this player
     }
 }
