56257f890cfb47bde87de77f240ee26b0a2881b1
Stefano Fiumara <stefano.fiumara@slalom.com>
2020-02-11 22:03:51 -0600

    Add AISystem (Stub)

---
+++ Assets/Scripts/HarryPotter/Systems/AISystem.cs
@@ -0,0 +1,17 @@
+using HarryPotter.Systems.Core;
+
+namespace HarryPotter.Systems
+{
+    public class AISystem : GameSystem, IAwake, IDestroy
+    {
+        public void Awake()
+        {
+            
+        }
+
+        public void Destroy()
+        {
+            
+        }
+    }
+}
