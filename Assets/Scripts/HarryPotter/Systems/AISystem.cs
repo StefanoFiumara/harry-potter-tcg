5913335c9e86dfb19a98ce8e7a002cb819a185c6
Stefano Fiumara <stefano.fiumara@slalom.com>
2020-04-01 00:28:35 -0500

    AI: Playable Creatures

--- Assets/Scripts/HarryPotter/Systems/AISystem.cs
+++ Assets/Scripts/HarryPotter/Systems/AISystem.cs
@@ -1,10 +1,13 @@
 using System.Linq;
 using HarryPotter.Data;
+using HarryPotter.Data.Cards;
+using HarryPotter.Data.Cards.CardAttributes;
 using HarryPotter.Enums;
 using HarryPotter.GameActions;
 using HarryPotter.GameActions.PlayerActions;
 using HarryPotter.Systems.Core;
 using UnityEngine;
+using Utils;
 
 namespace HarryPotter.Systems
 {
@@ -24,9 +27,20 @@ namespace HarryPotter.Systems
                 Container.ChangeTurn();
             }
         }
-        
+
         public GameAction DecideAction(GameState gameState, Player player)
         {
+            if (player.Hand.Any(c => c.Data.Type == CardType.Creature))
+            {
+                foreach (var card in player.Hand.Where(c => c.Data.Type == CardType.Creature))
+                {
+                    if (IsCardPlayable(player, card))
+                    {
+                        return new PlayCardAction(card);
+                    }
+                }
+            }
+            
             if (player.Hand.Any(c => c.Data.Type == CardType.Lesson))
             {
                 var lesson = player.Hand.First(c => c.Data.Type == CardType.Lesson);
@@ -34,6 +48,29 @@ namespace HarryPotter.Systems
             }
             
             return new DrawCardsAction(player, 1, true);
+        }
+        
+        private bool IsCardPlayable(Player p, Card c)
+        {
+            return HasEnoughActions(p, c) && HasEnoughLessons(p, c);
+        }
+
+        private bool HasEnoughActions(Player p, Card c)
+        {
+            var actionCost = c.GetAttribute<ActionCost>();
+            return p.ActionsAvailable >= actionCost.Amount;
+        }
+
+        private bool HasEnoughLessons(Player p, Card c)
+        {
+            var lessonCost = c.GetAttribute<LessonCost>();
+
+            if (lessonCost != null)
+            {
+                return p.LessonCount >= lessonCost.Amount && p.LessonTypes.Contains(lessonCost.Type);
+            }
+
+            return true;
         }
     }
 }
