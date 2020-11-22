d428db4bd10d92ba9c295e89ab76286f8eb87f8a
Stefano Fiumara <stefano.fiumara@slalom.com>
2020-11-22 17:59:33 -0600

    split DamagePlayerOrCreatureAction into separate file

--- Assets/Scripts/HarryPotter/GameActions/Actions/DamageCreatureAction.cs
+++ Assets/Scripts/HarryPotter/GameActions/Actions/DamageCreatureAction.cs
@@ -2,21 +2,6 @@ using HarryPotter.Data.Cards;
 
 namespace HarryPotter.GameActions.Actions
 {
-    public class DamagePlayerOrCreatureAction : GameAction
-    {
-        public Card Source { get; }
-        public Card Target { get; }
-        public int Amount { get; }
-        
-        public DamagePlayerOrCreatureAction(Card source, Card target, int amount)
-        {
-            Source = source;
-            Target = target;
-            Amount = amount;
-        }
-    }
-    
-    
     public class DamageCreatureAction : GameAction
     {
         public Card Source { get; }
---
+++ Assets/Scripts/HarryPotter/GameActions/Actions/DamagePlayerOrCreatureAction.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using HarryPotter.Data.Cards;
+using HarryPotter.Data.Cards.CardAttributes.Abilities;
+using HarryPotter.GameActions.ActionParameters;
+using HarryPotter.Systems.Core;
+
+namespace HarryPotter.GameActions.Actions
+{
+    public class DamagePlayerOrCreatureAction : GameAction, IAbilityLoader
+    {
+        public Card Source { get; private set; }
+        public Card Target { get; private set; }
+        public int Amount { get; private set; }
+        
+        public DamagePlayerOrCreatureAction(Card source, Card target, int amount)
+        {
+            Source = source;
+            Target = target;
+            Amount = amount;
+        }
+
+        public DamagePlayerOrCreatureAction()
+        {
+            
+        }
+        
+        public void Load(IContainer game, Ability ability)
+        {
+            var parameter = DamagePlayerOrCreatureParameter.FromString(ability.GetParams(nameof(DamagePlayerOrCreatureAction)));
+
+            Amount = parameter.Amount;
+            Source = ability.Owner;
+            Player = Source.Owner;
+
+            Target = ability.TargetSelector.SelectTargets(game).Single();
+        }
+    }
+}
