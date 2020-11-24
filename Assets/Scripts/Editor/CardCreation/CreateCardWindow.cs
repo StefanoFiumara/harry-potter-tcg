4e3bb2c1bead9a8ccb5f2d6f0016967db707ea53
Stefano Fiumara <stefano.fiumara@slalom.com>
2020-11-24 11:46:37 -0600

    update card library after card creation

--- Assets/Scripts/Editor/CardCreation/CreateCardWindow.cs
+++ Assets/Scripts/Editor/CardCreation/CreateCardWindow.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using HarryPotter.Data;
+using HarryPotter.Data.Cards;
 using HarryPotter.Data.Cards.CardAttributes;
 using HarryPotter.Data.Cards.CardAttributes.Abilities;
 using HarryPotter.Enums;
@@ -10,18 +12,19 @@ using UnityEngine;
 // ReSharper disable once CheckNamespace
 public class CreateCardWindow : EditorWindow
 {
+    private const string CARD_LIBRARY_PATH = "Assets/GameData/CardLibrary.asset";
+    
     [MenuItem("Harry Potter TCG/New Card Asset")]
     public static void NewCard()
     {
         GetWindow<CreateCardWindow>(true, "Create New Card", focus: true);
     }
 
-    private HarryPotter.Data.Cards.CardData _cardData;
-    
+    private CardData _cardData;
     
     private void OnEnable()
     {
-        _cardData = CreateInstance<HarryPotter.Data.Cards.CardData>();
+        _cardData = CreateInstance<CardData>();
 
         var actionCost = CreateInstance<ActionCost>();
         _cardData.Attributes.Add(actionCost);
@@ -67,11 +70,11 @@ public class CreateCardWindow : EditorWindow
         {
             AssetDatabase.AddObjectToAsset(attribute, assetPath);
 
-            if (attribute is HarryPotter.Data.Cards.CardAttributes.Abilities.Ability abilityAttribute)
+            if (attribute is Ability ability)
             {
-                if (abilityAttribute.TargetSelector != null)
+                if (ability.TargetSelector != null)
                 {
-                    AssetDatabase.AddObjectToAsset(abilityAttribute.TargetSelector, abilityAttribute);
+                    AssetDatabase.AddObjectToAsset(ability.TargetSelector, ability);
                 }
             }
         }
@@ -81,6 +84,22 @@ public class CreateCardWindow : EditorWindow
         // Reset Global Editor options
         AbilityEditor.SelectedTargetSelectorName = "None"; // TODO: Encapsulate this?
         Debug.Log($"Created Card Asset at: {AssetDatabase.GetAssetPath(_cardData)}");
+        
+        UpdateCardLibrary();
+    }
+
+    private static void UpdateCardLibrary()
+    {
+        var cardLibrary = AssetDatabase.LoadAssetAtPath<CardLibrary>(CARD_LIBRARY_PATH);
+
+        cardLibrary.Cards =
+            AssetDatabase.FindAssets($"t:{nameof(CardData)}", new[] {"Assets/GameData/Cards"})
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<CardData>)
+                .OrderBy(c => c.Type).ThenBy(c => c.CardName)
+                .ToList();
+
+        EditorUtility.SetDirty(cardLibrary);
     }
 
     private bool IsCardDataValid()
