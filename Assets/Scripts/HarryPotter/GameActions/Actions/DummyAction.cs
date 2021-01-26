d70a02219fde105b9ab355be240b239e3203189b
Stefano Fiumara <c-stefano.fiumara@charter.com>
2021-01-26 00:42:57 -0600

    add Dummy Action

---
+++ Assets/Scripts/HarryPotter/GameActions/Actions/DummyAction.cs
@@ -0,0 +1,13 @@
+using HarryPotter.Data.Cards.CardAttributes.Abilities;
+using HarryPotter.Systems.Core;
+
+namespace HarryPotter.GameActions.Actions
+{
+    public class DummyAction : GameAction, IAbilityLoader
+    {
+        public void Load(IContainer game, Ability ability)
+        {
+            // Do nothing
+        }
+    }
+}
