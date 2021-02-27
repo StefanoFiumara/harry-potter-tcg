0a0ce18d85846e574e3effd2b9a8c3c2e2f1a3ae
Stefano Fiumara <sdf10517@gmail.com>
2021-02-26 23:25:14 -0600

    remove unused attribute

--- Assets/Scripts/HarryPotter/Data/Cards/CardAttributes/RestrictionAttribute.cs
+++
@@ -1,9 +0,0 @@
-using HarryPotter.Systems.Core;
-
-namespace HarryPotter.Data.Cards.CardAttributes
-{
-    public abstract class RestrictionAttribute : CardAttribute
-    {
-        public abstract bool MeetsRestriction(IContainer game);
-    }
-}
